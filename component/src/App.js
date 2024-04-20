7d148855cfd38e00277c9563f3a255821d98223b
cagdas.sarigil <cagdas.sarigil@basarsoft.com.tr>
2024-04-20 17:33:59 +0300

    udemy component

--- component/src/App.js
+++ component/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import Course from "./components/Course";
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import CSharp from "./images/ccsharp.png";
+import Web from "./images/kompleweb.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarım</p>
+        </div>
+      </section>
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column">
+              <Course
+                image={Angular}
+                title="Angular"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Bootstrap}
+                title="Bootstrap 5"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={CSharp}
+                title="C#"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Web}
+                title="Web"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
